f371344bab6c2e174a505078c5bdc34a069e00cc
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-04-21 15:48:28 +0200

    Bump module to 1.1 and update changelog and icon key

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,17 @@
 # CHANGELOG MODULE PLANNINGINTERVENTION FOR [DOLIBARR ERP CRM](https://www.dolibarr.org)
 
+## 1.1
+
+- Ajout d'un extrafiled "Date de fin prévue"
+- Ajout d'un extrafield "Ignorer les heures d'ouvertures"
+- Support des horaires d'intervention
+- Support des horaires de dépassement
+- Refonte des traductions
+- Ajout des vues Jour/Semaine
+- Ajout d'un réglage "Masquer les horaires non travaillées (Jour/Semaine)"
+- Ajout du Support des Interventions sur plusieurs jours
+- Ajout d'une permission "Lire son planning d'intervention et celui de ses subordonnées"
+
 ## 1.0
 
 Initial version
--- core/modules/modPlanningIntervention.class.php
+++ core/modules/modPlanningIntervention.class.php
@@ -76,7 +76,7 @@ class modPlanningIntervention extends DolibarrModules
 		$this->editor_squarred_logo = '';					// Must be image filename into the module/img directory followed with @modulename. Example: 'myimage.png@planningintervention'
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated', 'experimental_deprecated' or a version string like 'x.y.z'
-		$this->version = '1.0.1';
+		$this->version = '1.1';
 		// Url to the file with your last numberversion of this module
 		//$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
@@ -87,7 +87,7 @@ class modPlanningIntervention extends DolibarrModules
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
 		// To use a supported fa-xxx css style of font awesome, use this->picto='xxx'
-		$this->picto = 'LogoFL@planningof';
+		$this->picto = 'LogoFL@planningintervention';
 
 		// Define some features supported by module (triggers, login, substitutions, menus, css, etc...)
 		$this->module_parts = array(
