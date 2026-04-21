b6331c11fd6f5a30e29a10680bcd058534319cb2
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-04-21 15:09:09 +0200

    Update modPlanningIntervention.class.php

--- core/modules/modPlanningIntervention.class.php
+++ core/modules/modPlanningIntervention.class.php
@@ -535,7 +535,7 @@ class modPlanningIntervention extends DolibarrModules
 				'param' => '',
 				'alwayseditable' => 1,
 				'perms' => '',
-				'list' => '3'
+				'list' => '3',
 				'help' => 'PlannedStartDateHelp',
 				'computed' => '',
 				'entity' => '',
